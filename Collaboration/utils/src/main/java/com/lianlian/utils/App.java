6e7c5ed03fbaa08a7bc31e42853864501a7f8eaf
dengxf <dengxf@windows10.microdone.cn>
2017-07-27 15:45:04 +0800

    框架目录

---
+++ Collaboration/utils/src/main/java/com/lianlian/utils/App.java
@@ -0,0 +1,13 @@
+package com.lianlian.utils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
